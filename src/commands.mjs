10c95780145662ca04abb8f12bd0bb25ab5e8165
Dolph <dolphalala@gmail.com>
2026-06-23 15:32:13 -0600

    Add iloveyou slash command

--- src/commands.mjs
+++ src/commands.mjs
@@ -2,5 +2,9 @@ export const commands = [
   {
     name: 'ping',
     description: 'Replies with bot latency.'
+  },
+  {
+    name: 'iloveyou',
+    description: 'Sends a random love letter for Lana and Allen.'
   }
 ];
