0b538c26e58a4788cfc80f2c14bfc72fda53d0ce
Dolph <dolphalala@gmail.com>
2026-06-23 15:39:17 -0600

    Limit commands to guild install context

--- src/commands.mjs
+++ src/commands.mjs
@@ -1,10 +1,14 @@
 export const commands = [
   {
     name: 'ping',
-    description: 'Replies with bot latency.'
+    description: 'Replies with bot latency.',
+    integration_types: [0],
+    contexts: [0]
   },
   {
     name: 'iloveyou',
-    description: 'Sends a random love letter for Lana and Allen.'
+    description: 'Sends a random love letter for Lana and Allen.',
+    integration_types: [0],
+    contexts: [0]
   }
 ];
